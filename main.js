5b3ff1ec1620dc1f984abc09c70072fb7fd05977
nelsonic <contact.nelsonic+github@gmail.com>
2016-04-11 10:57:12 +0100

    split out functions into /lib

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ process.env.GEONAMES_USERNAMES = 'numo,numo1,numo2,numo3,numo4,numo5'; // a few 
 var fs = require('fs');
 var geonames = require('lambda-taggable-geonames-indexer');
 var format_ne_hotel_as_taggable_tag = require('./lib/format_ne_hotel_as_taggable_tag');
+var lambda_taggable_create_document = require('./lib/lambda_taggable_create_document');
+var format_master_hotel_record = require('lib/format_master_hotel_record_as_taggable_tag.js')
 
 // Load the full list of NE Hoteles which have Package Holidays
 var all_ne_hotels = require('./data/all_ne_hotels.json');
@@ -10,45 +12,14 @@ console.log('All NE Hotels with Packages:', Object.keys(all_ne_hotels).length);
 
 var ne_hotel_ids = Object.keys(all_ne_hotels); // Array of Ids so we can itterate
 
-var record = format_ne_hotel_as_taggable_tag (all_ne_hotels['4473']);
-console.log(JSON.stringify(record, null, 2));
+var ne_hotel_record = format_ne_hotel_as_taggable_tag (all_ne_hotels['4473']);
+console.log(JSON.stringify(ne_hotel_record, null, 2));
 
-var AWS = require('aws-sdk');
-AWS.config.region = 'eu-west-1';
-var lambda = new AWS.Lambda();
-
-function save(record, type) {
-  var filename = './' + type + '/' + record._id.replace(':', '_') + '.json';
-  fs.writeFile(filename, JSON.stringify(record, null, 2), function(err, data){
-    // console.log(err, data);
-  });
+// only lookup & format Master Hotel Record if a mapping exists
+if(ne_hotel_record.tags.length > 0 && ne_hotel_record.tags[0].tagId.indeOf('NO_MHID') === -1) {
+  var master_hotel_record = format_master_hotel_record(ne_hotel_record);
 }
 
-function insert (record, callback) {
-  var params = {
-    FunctionName: 'lambda-taggable-createdocument-v1', // lambda function to invoke
-    InvocationType: 'RequestResponse',
-    LogType: 'Tail',
-    Payload: JSON.stringify(record)
-  };
-
-  lambda.invoke(params, function(err, data) {
-    save(record, 'records');
-    if (err) {
-      console.log(' - - - - - - - - - - - - - - - ERROR:')
-      console.log(err);
-      err._id = record._id;
-      save(err, 'errors');
-    } else {
-      console.log(data);
-      console.log(record._id);
-      data._id = record._id;
-      save(data, 'responses');
-      if(record._id.indexOf('hotel:NE') > -1) {
-        // return next();
-      }
-    }
-  });
-}
-
-insert(record);
+// lambda_taggable_create_document(record, function (err, data) {
+//   console.log(err, data)
+// });
